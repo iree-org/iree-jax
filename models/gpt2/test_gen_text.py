946d3a1b84ee2e3919e5df8d123b7e1644921ee4
Yi Wang <yi.wang.2005@gmail.com>
2023-02-26 21:00:43 -0800

    Use transformer tokenizer

--- models/gpt2/test_gen_text.py
+++ models/gpt2/test_gen_text.py
@@ -4,36 +4,32 @@ import jax.numpy as jnp
 import os
 import sys
 
-sys.path.append(
-    os.path.realpath(
-        os.path.join(
-            sys.path[0],
-            "../../../huggingface-tokenizer-in-cxx/tokenizer")))
-import bpe
+from transformers import GPT2Tokenizer
+
+tknzr = GPT2Tokenizer.from_pretrained("gpt2")
 
 import model
 import config
 
-assert_dir=os.path.join(sys.path[0], "assets")
+assert_dir = os.path.join(sys.path[0], "assets")
 
-tknzr = bpe.Tokenizer(assert_dir)
 ids = tknzr.encode("Hello, my dog is cute")
 
 params = model.load_gpt2_model("gpt2", assert_dir)
 
 cfg = config.get_config()
-B = cfg.B # Batch size
-K = cfg.K # Input sequence length
+B = cfg.B  # Batch size
+K = cfg.K  # Input sequence length
 S = cfg.S
-T = cfg.T # Batched decode
+T = cfg.T  # Batched decode
 L, _, _, Q, H, _ = model.model_sizes["gpt2"]
 
 nids = len(ids)
-t = jnp.asarray([nids])
-prompt = jnp.asarray([ids + [0]*(K-nids)], dtype=jnp.int32)
+t = jnp.asarray([nids], dtype=jnp.int32)
+prompt = jnp.asarray([ids + [0] * (K - nids)], dtype=jnp.int32)
+# t = [6]
+# prompt = [[15496    11   616  3290   318 13779     0     0]]
 
-print(f"t={t}")
-print(f"prompt={prompt}")
 
 kv = model.init_kv(1, S, L, Q, H, dtype=jnp.float32)
 kv = [jnp.tile(k, (1, prompt.shape[0], 1, 1, 1)) for k in kv]
@@ -44,6 +40,7 @@ for _ in range(12):
     kv, x = model.decode(params, kv, x, t)
     t = t + 1
     ids.append(x.item())
-
-print(f"result={ids}")
-assert tknzr.decode(ids) == "Hello, my dog is cute. I'm not sure if she's a puppy or not."
+# ids = [15496, 11, 616, 3290, 318, 13779, 13, 314, 1101, 407, 1654, 611, 673, 338, 257, 26188, 393, 407, 13]
+assert (
+    tknzr.decode(ids) == "Hello, my dog is cute. I'm not sure if she's a puppy or not."
+)

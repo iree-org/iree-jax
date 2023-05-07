f27cb52fdfc29bc76cc1e1d6b85b42fcfd1c835d
Yi Wang <yi.wang.2005@gmail.com>
2023-05-06 18:35:06 -0700

    Add -C - option to curl when downloading

--- models/gpt2/setup.sh
+++ models/gpt2/setup.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 mkdir -p assets
-curl -L https://huggingface.co/gpt2/raw/main/vocab.json -o assets/vocab.json
-curl -L https://huggingface.co/gpt2/raw/main/tokenizer.json -o assets/tokenizer.json
-curl -L https://huggingface.co/gpt2/raw/main/merges.txt -o assets/merges.txt
-curl -L https://huggingface.co/gpt2/resolve/main/tf_model.h5 -o assets/tf_model.h5
+curl -C - -L https://huggingface.co/gpt2/raw/main/vocab.json -o assets/vocab.json
+curl -C - -L https://huggingface.co/gpt2/raw/main/tokenizer.json -o assets/tokenizer.json
+curl -C - -L https://huggingface.co/gpt2/raw/main/merges.txt -o assets/merges.txt
+curl -C - -L https://huggingface.co/gpt2/resolve/main/tf_model.h5 -o assets/tf_model.h5

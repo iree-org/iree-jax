584b6c1737a335a73feeb6c08f77eb306eda4477
Jacques Pienaar <jpienaar@google.com>
2023-09-01 10:13:38 -0700

    Update license file
    This has been pending for a bit, use Apache 2 with LLVM exceptions for
    consistency.

--- models/gpt2/config.py
+++ models/gpt2/config.py
@@ -1,3 +1,8 @@
+# Copyright 2023 The IREE Authors
+#
+# Licensed under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 """Flags and model configuration details, useful for replicating across files."""
 from absl import flags
 from os import path
